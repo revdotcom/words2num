b7453f697b79a2e95a567b398cdd900b9ea6fa8f
Joshua Dong <jdong42@gmail.com>
2017-04-25 11:20:35 -0500

    add tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -20,13 +20,15 @@ class TestITN(unittest.TestCase):
 
 
     def test_en_us(self):
-        """Test postives for en-US.
+        """Test valid en-US input.
         """
         test_trials = ("two",
             "twelve",
             "one hundred and two",
             "a hundred twenty-three",
+            "thirty-four hundred fifty-six",
             "two hundred thousand, three hundred forty five",
+            "twenty-three hundred thousand four",
             "sixty-eight billion, two hundred two million and two",
             "sixty eight billion two hundred two million two",
             "nine trillion eight hundred seven")
@@ -34,14 +36,38 @@ class TestITN(unittest.TestCase):
             12,
             102,
             123,
+            3456,
             200345,
+            2300004,
             68202000002,
             68202000002,
             9000000000807)
         tests = zip(test_trials, test_targets)
 
         for (trial, target) in tests:
-            assert word2num.word2num(trial) == target
+            assert word2num.word2num(trial) == target,\
+                   "failed to parse '{0}' as {1}".format(trial, target)
+
+
+    def test_en_us_neg(self):
+        """Test invalid en-US input.
+        """
+        tests = ("seven eleven",
+            "one one",
+            "one one one",
+            "one one one one",
+            "one one one one one",
+            "one million one hundred one one",
+            "one thousand one million one",
+            "one thousand hundred million")
+
+        for test in tests:
+            try:
+                value = word2num.word2num(test)
+                assert False,\
+                       "parsed invalid input '{0}' as {1}".format(test, value)
+            except word2num.NumberParseException:
+                pass
 
 
 if __name__ == '__main__':
--- word2num/__init__.py
+++ word2num/__init__.py
@@ -1,2 +1,2 @@
 from .word2num import (word2num, NumberParseException)
-__version__ = '0.0.2'
+__version__ = '0.0.4'
